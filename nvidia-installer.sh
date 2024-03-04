43084aee83a51ab1b807f2c00892a0d0b1d9cd67
r4t1n <r4t1n@proton.me>
2024-03-04 16:07:45 +0100

    Create nvidia-installer.sh

---
+++ nvidia-installer.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+default_cdn="us"
+
+if [ $# -eq 0 ]; then
+    echo "Usage: $0 [--cdn <CDN>] <NVIDIA Driver Version>"
+    exit 1
+fi
+
+while [[ $# -gt 0 ]]; do
+    key="$1"
+
+    case $key in
+        --cdn)
+            cdn="$2"
+            shift
+            shift
+            ;;
+        *)
+            version="$1"
+            shift
+            ;;
+    esac
+done
+
+if [ -z "$cdn" ]; then
+    cdn="$default_cdn"
+fi
+
+url="https://${cdn}.download.nvidia.com/XFree86/Linux-x86_64/${version}/NVIDIA-Linux-x86_64-${version}.run"
+wget "$url"
+
+chmod +x NVIDIA-Linux-x86_64-${version}.run
+./NVIDIA-Linux-x86_64-${version}.run
